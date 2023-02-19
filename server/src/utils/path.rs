1ff4db64b2c3a9f6104d5d49aca5d286cc55669f
Tony Hu <hujingyuan25@163.com>
2023-02-19 20:29:43 +0800

    feat: ignore symlink in secure join

--- server/src/utils/path.rs
+++ server/src/utils/path.rs
@@ -3,6 +3,20 @@ use super::error::AppError;
 
 pub fn secure_join(root: &PathBuf, unsafe_path: &PathBuf) -> Result<PathBuf, AppError> {
   if unsafe_path.has_root() {
+    return Err(AppError::new("path error: path has root"));
+  }
+  if let Some(s) = unsafe_path.to_str() {
+    if s.contains("..") {
+      return Err(AppError::new("path error"));
+    }
+  }
+  let new_path = root.join(unsafe_path);
+  Ok(new_path)
+}
+
+#[allow(unused)]
+pub fn secure_join_symlink(root: &PathBuf, unsafe_path: &PathBuf) -> Result<PathBuf, AppError> {
+  if unsafe_path.has_root() {
     return Err(AppError::new("path error"));
   }
   let new_path = root.join(unsafe_path);
