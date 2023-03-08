5b76e3840c2a1d1a2bea6a906314e50ca9e3dcec
Tony Hu <hujingyuan25@163.com>
2023-03-08 18:17:38 +0800

    fix: secure path join

--- server/src/utils/path.rs
+++ server/src/utils/path.rs
@@ -3,11 +3,11 @@ use super::error::AppError;
 
 pub fn secure_join(root: &PathBuf, unsafe_path: &PathBuf) -> Result<PathBuf, AppError> {
   if unsafe_path.has_root() {
-    return Err(AppError::new("path error: path has root"));
+    return Err(AppError::new(&format!("path error: path has root {:?}", unsafe_path)));
   }
   if let Some(s) = unsafe_path.to_str() {
     if s.contains("..") {
-      return Err(AppError::new("path error"));
+      return Err(AppError::new(&("path error: ".to_owned() + s)));
     }
   }
   let new_path = root.join(unsafe_path);
@@ -17,7 +17,7 @@ pub fn secure_join(root: &PathBuf, unsafe_path: &PathBuf) -> Result<PathBuf, App
 #[allow(unused)]
 pub fn secure_join_symlink(root: &PathBuf, unsafe_path: &PathBuf) -> Result<PathBuf, AppError> {
   if unsafe_path.has_root() {
-    return Err(AppError::new("path error"));
+    return Err(AppError::new(&format!("path error: path has root {:?}", unsafe_path)));
   }
   let new_path = root.join(unsafe_path);
   let t1 = new_path.canonicalize()?;
@@ -27,7 +27,7 @@ pub fn secure_join_symlink(root: &PathBuf, unsafe_path: &PathBuf) -> Result<Path
   let rc = t2.components();
   
   if nc.into_iter().count() < rc.into_iter().count() {
-    return Err(AppError::new("path error"));
+    return Err(AppError::new(&format!("path error: {:?}", unsafe_path)));
   }
   Ok(new_path)
 }
